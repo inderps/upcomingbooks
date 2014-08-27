39d860ee0badf6b752162d28b1f1c600b4f5e75b
Inderpal Singh <inderpal@Admins-MacBook-Pro-5.local>
2014-08-28 01:37:14 +0530

    improved layout a bit

--- api/controllers/BookController.js
+++ api/controllers/BookController.js
@@ -23,18 +23,18 @@ module.exports = {
    * `BookController.show()`
    */
   show: function (req, res) {
-      Author.create({name: 'ips'}).exec(function createCB(err, created){
-          console.log(created.name);
-      });
-      Author.find({}).exec(function findCB(err, found){
-//          var user = null;
-//        while(found.length){
-//            user = found.pop().name;
-//        }
-//          res.render('book/show', {
-//              user: user
-//          });
-      });
+//      Author.create({name: 'ips'}).exec(function createCB(err, created){
+//          console.log(created.name);
+//      });
+//      Author.find({}).exec(function findCB(err, found){
+////          var user = null;
+////        while(found.length){
+////            user = found.pop().name;
+////        }
+////          res.render('book/show', {
+////              user: user
+////          });
+//      });
     console.log("ss");
 //    return res.json({
 //      todo: 'show() is not implemented yet!'
